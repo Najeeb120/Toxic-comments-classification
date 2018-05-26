2b6a2225c676a38bf417d561f5b6c5c9c98fe124
Najeeb Khan <najeebnjk20@yahoo.com>
2018-05-26 18:20:07 +0530

    Update training.py

--- training.py
+++ training.py
@@ -1,156 +1,174 @@
-import pandas as pd
 import numpy as np
-from keras.models import load_model
-from sklearn.metrics import roc_auc_score, log_loss
-from sklearn.model_selection import KFold, train_test_split
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-from tools.utils import format_time, create_submission, average_predictions, \
-        geom_average_predictions, read_yaml
-from nn_utils.models import instantiate_model
-from nn_utils import preprocessing, running
+import keras
+from keras import optimizers
+from keras import backend as K
+from keras import regularizers
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout, Flatten
+from keras.layers import Embedding, Conv1D, MaxPooling1D, GlobalMaxPooling1D
+from keras.utils import plot_model
+from keras.preprocessing import sequence
+from keras.preprocessing.text import Tokenizer
+from keras.callbacks import EarlyStopping
+In [ ]:
+from tqdm import tqdm
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+import os, re, csv, math, codecs
 
-import argparse
-import sys
-import time
+sns.set_style("whitegrid")
+np.random.seed(0)
 
-SEED = 2610
-np.random.seed(SEED)
-labels = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+DATA_PATH = '../input/'
+EMBEDDING_PATH = '../input/'
 
-parser = argparse.ArgumentParser(description="Recurrent neural network for \
-                        identifying and classifying toxic online comments")
-parser.add_argument("embedding_file", nargs='?', default='input/sample_fasttext.vec',
-                        help='Pre-trained embedding file path')
-parser.add_argument("-m", "--model", default=None, help='Type of architecture to be trained')
+MAX_NB_WORDS = 100000
+tokenizer = RegexpTokenizer(r'\w+')
+stop_words = set(stopwords.words('english'))
+stop_words.update(['.', ',', '"', "'", ':', ';', '(', ')', '[', ']', '{', '}'])
 
-args = parser.parse_args()
+from subprocess import check_output
+print(check_output(["ls", "../input"]).decode("utf8"))
+In [ ]:
+# Load Embedding
+print('load word embedding...')
+embedding_index = {}
+f = codecs.open('../input/fasttext/wiki.simple1.vec', encoding='utf-8')
+for line in tqdm(f):
+    values = line.rstrip().rsplit(' ')
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embedding_index[word] = coefs
+f.close()
+print('found %s word vectors' % len(embedding_index))
+In [ ]:
+# Load Data
+input_dir = '../input/jigsaw-toxic-comment-classification-challenge/'
+train_df = pd.read_csv(input_dir + 'train.csv', sep=',', header=0)
+test_df = pd.read_csv(input_dir + 'test.csv', sep=',', header=0)
 
-print('Parsing parameters')
-embedding_file = args.embedding_file
+test_df = test_df.fillna('_NA_')
+print('Training DF: '+str(train_df.shape))
+print('Testing DF: '+str(test_df.shape))
 
-if args.model:
-    model_name = args.model
-    try:
-        config = read_yaml('best_configs/{}.yaml'.format(model_name))
-        print('Best parameters for {} loaded!'.format(model_name))
-    except FileNotFoundError:
-        print('Wrong model type!')
-        sys.exit(1)
-else:
-    config = read_yaml('parameters.yaml')
-    print('Custom parameters loaded!')
+label_names = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
+y_train = train_df[label_names].values
 
-FILENAME = config.FILENAME
-MODEL_TYPE = config.MODEL_TYPE
-TRAINING_PARAMS = config.training_parameters
-MODEL_PARAMS = config.model_parameters
-PREPROCESSING_PARAMS = config.preprocessing_parameters
+print(y_train.shape)
 
-max_sequence_length = PREPROCESSING_PARAMS.max_sequence_length
-max_nb_words = PREPROCESSING_PARAMS.max_nb_words
+# Visualize
+train_df['doc_len'] = train_df['comment_text'].apply(lambda words: len(words.split(" ")))
+max_seq_len = np.round(train_df['doc_len'].mean() + train_df['doc_len'].std()).astype(int)
+sns.distplot(train_df['doc_len'], hist=True, kde=True, color='b', label='doc_len')
+plt.axvline(x=max_seq_len, color='k', linestyle='--', label='max len')
+plt.title('comment length');
+plt.legend()
+plt.show()
+In [ ]:
+raw_docs_train = train_df['comment_text'].tolist()
+raw_docs_test = test_df['comment_text'].tolist()
+num_classes = len(label_names)
 
-print('Loading and preprocessing data')
-df, df_test = preprocessing.load_data(PREPROCESSING_PARAMS, embedding_file)
+print('pre-processing training data')
+processed_train_data = []
+for doc in tqdm(raw_docs_train):
+    tokens = tokenizer.tokenize(doc)
+    filtered = [word for word in tokens if word not in stop_words]
+    processed_train_data.append(" ".join(filtered))
 
-"""Classic NLP preprocessing for Keras"""
-pp = preprocessing.Preprocessor('comment_text', PREPROCESSING_PARAMS)
+print('pre-processing test data')
+processed_test_data = []
+for doc in tqdm(raw_docs_test):
+    tokens = tokenizer.tokenize(doc)
+    filtered = [word for word in tokens if word not in stop_words]
+    processed_test_data.append(" ".join(filtered))
 
-df, df_test = pp.fill_null(df, df_test)
-y_train = df[labels].values
+print("Tokenizing input data....")
+tokenizer = Tokenizer(num_words=MAX_NB_WORDS, lower=True, char_level=False)
+tokenizer.fit_on_texts(processed_train_data + processed_test_data)
+word_seq_train = tokenizer.texts_to_sequences(processed_train_data)
+word_seq_test = tokenizer.texts_to_sequences(processed_test_data)
+word_index = tokenizer.word_index
+print("Dictionary Size: "+str(len(word_index)))
 
-pp.set_tokenizer(df, df_test, fit_on_train_only=False)
-x_train, x_test = pp.tokenize_and_pad(df, df_test)
-print('Shape of train tensor ', x_train.shape)
-print('Shape of test tensor ', x_test.shape)
+# Pad Sequences
+word_seq_train = sequence.pad_sequences(word_seq_train, maxlen=max_seq_len)
+word_seq_test = sequence.pad_sequences(word_seq_test, maxlen=max_seq_len)
+In [ ]:
+# Training Parameters
+batch_size = 256
+num_epochs = 8
 
-#Pre-trained embeddings
-print('Creating embedding matrix...')
-begin_matrix = time.time()
-embedding_matrix, embedding_dimension = pp.make_words_vec(embedding_file)
-end_matrix = format_time(time.time() - begin_matrix)
-print('Matrix created - shape: {} - time: {}'.format(embedding_matrix.shape, end_matrix))
+num_filters = 64
+embed_dim = 300
+weight_decay = 1e-4
+In [ ]:
+# Embedding Matrix
+words_not_found = []
+nb_words = min(MAX_NB_WORDS, len(word_index))
+embedding_matrix = np.zeros((nb_words, embed_dim))
 
-print('\nStart training of {} model!'.format(MODEL_TYPE))
-"""Fit on 90% of the dataset"""
-if config.run_90p:
+for word, i in word_index.items():
+    if(i>=nb_words):
+        continue
+    embedding_vector = embedding_index.get(word)
+    if(embedding_vector is not None) and len(embedding_vector)>0:
+        embedding_matrix[i] = embedding_vector
+    else:
+        words_not_found.append(word)
+print('Number of null words embeddings found %d' % np.sum(np.sum(embedding_matrix, axis=1)==0))
+In [ ]:
+print(' Sample words not found: ',np.random.choice(words_not_found, 10))
+In [ ]:
+print('Building CNN')
+model = Sequential()
+model.add(Embedding(nb_words, embed_dim, weights=[embedding_matrix], input_length=max_seq_len, trainable=False))
+model.add(Conv1D(num_filters, 7, activation='relu', padding='same'))
+model.add(MaxPooling1D(2))
+model.add(Conv1D(num_filters, 7, activation='relu', padding='same'))
+model.add(GlobalMaxPooling1D())
+model.add(Dropout(0.5))
+model.add(Dense(32, activation='relu', kernel_regularizer=regularizers.l2(weight_decay)))
+model.add(Dense(num_classes, activation='sigmoid'))
 
-    begin_cv = time.time()
-    model = instantiate_model(MODEL_TYPE, MODEL_PARAMS, max_sequence_length, max_nb_words, embedding_dimension, embedding_matrix)
-    print(model.summary())
-
-    X_train, X_valid, Y_train, Y_valid = train_test_split(x_train, y_train, test_size = 0.1, random_state = SEED)
-    #filepath = 'temporary_cv_models/model-{}-autocheckpoint.h5'.format(FILENAME)
-    filepath = 'model-{}-autocheckpoint.h5'.format(FILENAME)
-    print('Fitting model on 90% of the data...')
-    hist, stopped_epoch = running.fitting_model(model, X_train, Y_train, X_valid, Y_valid, TRAINING_PARAMS, filepath)
-    end_cv = format_time(time.time() - begin_cv)
-
-    print('Training complete - {} - time: {}'.format(FILENAME, end_cv))
-    auc = hist['roc_auc_val'][stopped_epoch-TRAINING_PARAMS.patience]
-    loss = hist['val_loss'][stopped_epoch-TRAINING_PARAMS.patience]
-    print('AUC:', round(auc,5), ' / Loss:', round(loss,5))
-
-    print('Generating predictions from the best model...')
-    model.load_weights(filepath)
-    preds_ = model.predict(x_test, batch_size = 2*TRAINING_PARAMS.batch_size, verbose = 1)
-    create_submission(preds_, '{}_cv_{}-ave_{:0.5f}.csv'.format(MODEL_TYPE, FILENAME, auc))
-
-"""Perform K fold CV"""
-if config.run_kfold:
-    kf = KFold(n_splits=config.n_folds, shuffle=True, random_state=SEED)
-    cv_scores = []
-    cv_losses = []
-    cv_predictions = []
-    oof_predictions = np.zeros((len(x_train), len(labels)))
-
-    begin_cv = time.time()
-    print('Performing Kfold cross validation...')
-    for i, (train, valid) in enumerate(kf.split(x_train)):
-        begin_fold = time.time()
-        model = instantiate_model(MODEL_TYPE, MODEL_PARAMS, max_sequence_length, max_nb_words, embedding_dimension, embedding_matrix)
-        #filepath = 'temporary_cv_models/model-{}.fold-{:d}-autocheckpoint.h5'.format(FILENAME, i+1)
-        filepath = 'model-{}.fold-{:d}-autocheckpoint.h5'.format(FILENAME, i+1)
-        if i == 0:
-            print(model.summary())
-        print('Starting fold {:d}...'.format(i+1))
-        hist, stopped_epoch = running.fitting_model(model, x_train[train], y_train[train], x_train[valid], y_train[valid], TRAINING_PARAMS, filepath)
-
-        end_fold = format_time(time.time() - begin_fold)
-
-        cv_scores.append(hist['roc_auc_val'][stopped_epoch-TRAINING_PARAMS.patience])
-        cv_losses.append(hist['val_loss'][stopped_epoch-TRAINING_PARAMS.patience])
-        print('--- Fold AUC {:.5f} - Stopped epoch: {:d} - time: {}'.format(cv_scores[i], stopped_epoch-TRAINING_PARAMS['patience']+1, end_fold))
-        print('--- Fold loss {:.5f}'.format(cv_losses[i]))
-        #load the best model to generate OOF predictions and test predictions
-        model.load_weights(filepath)
-        print('Saving OOF predictions...')
-        oof_predictions[valid, :] = model.predict(x_train[valid], batch_size = 2*TRAINING_PARAMS.batch_size, verbose = 1)
-        #pd.DataFrame(oof_predictions).to_csv('temporary_oof_preds/temp_oof_preds_'+FILENAME+'.csv',index=False)
-        pd.DataFrame(oof_predictions).to_csv('temp_oof_preds_'+FILENAME+'.csv',index=False)
-
-        print('Generating test predictions from the best model...')
-        preds_ = model.predict(x_test, batch_size = 2*TRAINING_PARAMS.batch_size, verbose = 1)
-        cv_predictions.append(preds_)
-        #pd.DataFrame(preds_).to_csv('temporary_cv_preds/preds-{}.fold-{:d}.csv'.format(FILENAME,i+1), index = False)
-        pd.DataFrame(preds_).to_csv('preds-{}.fold-{:d}.csv'.format(FILENAME,i+1), index = False)
-        print('\n')
-        del model
-
-    end_cv = format_time(time.time() - begin_cv)
-    #pd.DataFrame(oof_predictions).to_csv('oof_predictions/oof_pred_'+FILENAME+'.csv', index = False)
-    pd.DataFrame(oof_predictions).to_csv('oof_pred_'+FILENAME+'.csv', index = False)
-    oof_auc = roc_auc_score(y_train, oof_predictions)
-
-    print('--- Results : {}\nAverage AUC {:.5f} +/- {:.4f} -- time: {}'.format(FILENAME, np.mean(cv_scores), np.std(cv_scores), end_cv))
-    print('Average loss {:.5f} +/- {:.4f}'.format(np.mean(cv_losses), np.std(cv_losses)))
-    print('Out of fold AUC {:.5f}'.format(oof_auc))
-
-    """Compute n-folds average test predictions"""
-    print('Saving predictions...')
-    preds = average_predictions(cv_predictions, config.n_folds)
-    create_submission(preds, '{}_cv_{}.csv'.format(MODEL_TYPE, FILENAME))
-
-    #"""Geometric mean"""
-    #print('Geometric')
-    #preds2 = geom_average_predictions(cv_predictions, config.n_folds)
-    #create_submission(preds2, '{}_cvgeom_{}.csv'.format(MODEL_TYPE, FILENAME))
+adam = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
+model.summary()
+In [ ]:
+# Callbacks
+early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=4, verbose=1)
+callback_list = [early_stopping]
+In [ ]:
+# Training CNN
+hist = model.fit(word_seq_train, y_train, batch_size=batch_size, epochs=num_epochs, callbacks=callback_list, validation_split=0.1, shuffle=True, verbose=2)
+In [ ]:
+y_test = model.predict(word_seq_test)
+In [ ]:
+#create a submission
+submission_df = pd.DataFrame(columns=['id'] + label_names)
+submission_df['id'] = test_df['id'].values 
+submission_df[label_names] = y_test 
+submission_df.to_csv("./submit.csv", index=False)
+In [ ]:
+# Generate Plots
+plt.figure()
+plt.plot(hist.history['loss'], lw=2.0, color='b', label='train')
+plt.plot(hist.history['val_loss'], lw=2.0, color='r', label='val')
+plt.title('CNN Toxic Sentiment')
+plt.xlabel('Epochs')
+plt.ylabel('Binary Cross-Entropy Loss')
+plt.legend(loc='upper right')
+plt.show()
+In [ ]:
+plt.figure()
+plt.plot(hist.history['acc'], lw=2.0, color='b', label='train')
+plt.plot(hist.history['val_acc'], lw=2.0, color='r', label='val')
+plt.title('CNN Toxic Sentiment')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc='upper left')
+plt.show()
